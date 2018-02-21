e9ebdf47f376d1e418aa68ac16278313814265fa
imqadir <alpha.imran@outlook.com>
2018-02-22 00:42:03 +0200

    file for phase one a

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,23 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  address: string = '';
+  apiKey:string = 'AIzaSyA3s9I9UoqZUT6-Q2pJ-hBnvYVhpoDYJdY';
+  latitude: number = 0;
+  longitude: number = 0;
+  status: string = '';
 
+  constructor(public navCtrl: NavController, private httpClient: HttpClient) {}
+
+  geocodeAddress(){
+    this.httpClient.get('https://maps.googleapis.com/maps/api/geocode/json?address='+ this.address + '&key=' + this.apiKey)
+    .subscribe(
+      (data:any) => {
+      this.status = data.status;
+      this.latitude = data.results[0].geometry.location.lat;
+      this.longitude = data.results[0].geometry.location.lng;
+  });
+
+  console.log(this.status);
   }
-
 }
